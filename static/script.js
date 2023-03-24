0b3a4e2f4d76841cddbd2e05ea645a333c4df594
Bogdan <bokizvezdara@gmail.com>
2023-03-24 19:15:53 +0100

    added page loader, still not completed fully

---
+++ static/script.js
@@ -0,0 +1,10 @@
+
+const spinnerWrapperElement = document.querySelector('.spinner-wrapper');
+
+window.addEventListener('load',()=>{
+    spinnerWrapperElement.style.opacity = '0';
+
+    setTimeout(()=>{
+        spinnerWrapperElement.style.display = 'none';
+    },500)
+})
